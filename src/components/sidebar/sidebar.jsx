552986f04808c5e08c81c885172886607654ab18
Nikola Djukic <nikola.djukic@amplitudo.me>
2021-04-02 15:55:00 +0200

    home page

---
+++ src/components/sidebar/sidebar.jsx
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams,
+} from "react-router-dom";
+import Home from "../home/home";
+
+function Sidebar() {
+  return (
+    <div>
+      <Router>
+        <Switch>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default Sidebar;
