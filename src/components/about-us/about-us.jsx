9483c0842bcda65b52c3d9480672a40d4d4ccfb8
Nikola Djukic <nikola.djukic@amplitudo.me>
2021-04-05 19:33:13 +0200

    about-us

--- src/components/about-us/about-us.jsx
+++ src/components/about-us/about-us.jsx
@@ -2,7 +2,55 @@ import React, { useState, useEffect } from "react";
 import "./about-us.scss";
 
 function AboutUs() {
-  return <div>About Us</div>;
+  return (
+    <div>
+      <div className="heading-small">
+        <h2 data-aos="flip-left" data-aos-duration="1000" data-aos-once="true">
+          O <span>nama</span>
+        </h2>
+      </div>
+      <div className="about-text">
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque
+          quaerat quibusdam sequi blanditiis omnis at nisi, velit, delectus
+          beatae facilis possimus neque error sunt. Corrupti quibusdam ab
+          corporis rem earum nobis culpa temporibus autem et, aliquid iusto
+          veritatis ipsa reiciendis dolore nemo vitae sequi aspernatur suscipit
+          exercitationem quasi optio perspiciatis.
+        </p>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex magni
+          dolorem molestias incidunt, ipsum error ipsam sapiente veniam facere
+          esse.
+        </p>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque
+          quaerat quibusdam sequi blanditiis omnis at nisi, velit, delectus
+          beatae facilis possimus neque error sunt. Corrupti quibusdam ab
+          corporis rem earum nobis culpa temporibus autem et, aliquid iusto
+          veritatis ipsa reiciendis dolore nemo vitae sequi aspernatur suscipit
+          exercitationem quasi optio perspiciatis.
+        </p>
+      </div>
+      <div class="mapouter">
+        <div class="gmap_canvas">
+          <iframe
+            width="100%"
+            height="500"
+            id="gmap_canvas"
+            src="https://maps.google.com/maps?q=podgorica&t=&z=19&ie=UTF8&iwloc=&output=embed"
+            frameborder="0"
+            scrolling="no"
+            marginheight="0"
+            marginwidth="0"
+          ></iframe>
+          <a href="https://grantorrent-es.com">grantorrent</a>
+          <br></br>
+          <a href="https://www.embedgooglemap.net">website maps free</a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default AboutUs;
--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -2,7 +2,11 @@ import React, { useState, useEffect } from "react";
 import "./contact.scss";
 
 function Contact() {
-  return <div>Contact</div>;
+  return (
+    <div>
+      
+    </div>
+  );
 }
 
 export default Contact;
